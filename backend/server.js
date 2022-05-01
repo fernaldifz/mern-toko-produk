997f6ed1daa7bbff66d91709299c0a9bf410dda2
fernaldifz <fernaldifz@gmail.com>
2022-05-01 19:38:49 +0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/products", products);
+app.use("/api/products", products);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
 export default app;
